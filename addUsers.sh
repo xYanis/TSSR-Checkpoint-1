bfa8ca0aff5929a6f628d77f75e860f291609fd5
xYanis <161461625+xYanis@users.noreply.github.com>
2024-03-31 14:45:47 +0200

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,20 +1,28 @@
 #!/bin/bash 
 
 # Vérification de la présence d'arguments
+
 if [ $# -eq 0 ]; then
     echo "Il manque les noms d'utilisateurs en argument - Fin du script"
     exit 1
 fi
 
 # Boucle sur les arguments pour créer les utilisateurs
+
 for username in "$@"; do
+
     # Vérification de l'existence de l'utilisateur
+    
     if id "$username" &>/dev/null; then
         echo "L'utilisateur $username existe déjà"
     else
+    
         # Création de l'utilisateur
+        
         sudo useradd -m "$username"
+        
         # Vérification de la création de l'utilisateur
+        
         if [ $? -eq 0 ]; then
             echo "L'utilisateur $username a été créé"
         else
